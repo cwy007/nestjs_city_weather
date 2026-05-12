2e1f1ef48d01845ffdad112ea1afb15f71fc4506
chanweiyan <chanweiyan007@gmail.com>
2026-05-12 19:41:14 +0800

    fix: convert gbk characterset

--- src/request-log.interceptor.ts
+++ src/request-log.interceptor.ts
@@ -1,19 +1,39 @@
 import {
   CallHandler,
   ExecutionContext,
+  Inject,
   Injectable,
   Logger,
   NestInterceptor,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
-import { Observable, tap } from 'rxjs';
+import { firstValueFrom, Observable, tap } from 'rxjs';
 import requestIp from 'request-ip';
+import { HttpService } from '@nestjs/axios';
+import * as iconv from 'iconv-lite';
 
 @Injectable()
 export class RequestLogInterceptor implements NestInterceptor {
   private readonly logger = new Logger(RequestLogInterceptor.name);
 
-  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+  @Inject(HttpService)
+  private readonly httpService: HttpService;
+
+  async ipToCity(ip: string): Promise<string> {
+    // curl - s "https://whois.pconline.com.cn/ipJson.jsp?ip=221.237.121.165&json=true"
+    const url = `https://whois.pconline.com.cn/ipJson.jsp?ip=${ip}&json=true`;
+    try {
+      const { data } = await firstValueFrom(this.httpService.get(url, { responseType: 'arraybuffer' }));
+      const decodedData = iconv.decode(Buffer.from(data), 'GBK');
+      const json = JSON.parse(decodedData);
+      return json?.addr;
+    } catch (error) {
+      this.logger.error(`Failed to fetch city for IP ${ip}: ${error.message}`);
+      return 'Unknown';
+    }
+  }
+
+  async intercept(context: ExecutionContext, next: CallHandler): Promise<Observable<any>> {
     const request = context.switchToHttp().getRequest<Request>();
     const response = context.switchToHttp().getResponse<Response>();
 
@@ -21,6 +41,9 @@ export class RequestLogInterceptor implements NestInterceptor {
 
     const { method, path, ip } = request;
     const clientIp = requestIp.getClientIp(request);
+
+    const city = await this.ipToCity('221.237.121.165');
+    console.log(`Client IP: ${clientIp}, City: ${city}`);
 
     this.logger.debug(
       `${method} ${path} - ${clientIp} ${userAgent} ${context.getClass().name}.${context.getHandler().name} invoked...`,
