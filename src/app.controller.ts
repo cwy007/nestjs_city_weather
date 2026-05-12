1707fea24c55c895a9399d5d7ab579589cacb7c0
chanweiyan <chanweiyan007@gmail.com>
2026-05-12 17:47:17 +0800

    fix: test pinyin pkg

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('pinyin')
+  getPinyin(@Query('text') text: string): string {
+    return this.appService.getPinyin(text);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import pinyin from 'pinyin';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getPinyin(text: string): string {
+    return pinyin(text, { style: pinyin.STYLE_NORMAL }).join('');
   }
 }
